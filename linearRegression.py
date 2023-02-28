116b510cd5ca6933ae29d83a4338b9a24b0696c0
New User <shankarcat@yahoo.com>
2023-02-28 00:23:25 -0500

    Final working regression

--- linearRegression.py
+++ linearRegression.py
@@ -1,6 +1,7 @@
 # Import libraries
 import pandas as pd
 import numpy as np
+import matplotlib
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
